2ad3377fd8f7884ff7cf1e4e4804cb226a90f364
Unknown <jua16073@uvg.edu.gt>
2018-08-27 16:56:30 -0600

    store?

--- lista/src/App.js
+++ lista/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import compra from './Components/ComprasApp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hey
-        hola
+        <compra />
       </div>
     );
   }
--- lista/src/Components/ComprasApp/index.js
+++ lista/src/Components/ComprasApp/index.js
@@ -1,16 +1,44 @@
-import {createStore, combineReucer} from 'redux';
+import {createStore, combineReducers} from 'redux';
+
 
 const addNewItem = (nombre,precio, cantidad) => ({
   type: 'NEW_ITEM_ADDED',
   payload: nombre, precio, cantidad,
 })
 
+const isAdded = (items = [], action) => {
+  switch(action.type){
+    case 'NEW_ITEM_ADDED':{
+      return items;
+    }
+    default:{
+      return items;
+    }
+  }
+};
+
 const addToList = (id) => ({
   type: 'ADDED_TO_LIST',
   payload: id,
 })
 
+const newItem = (state = [], action) => {
+  switch(action.type){
+    case 'ADDED_TO_LIST':{
+      return state;
+    }
+    default:{
+      return state;
+    }
+  }
+};
 
+const reducer = combineReducers(
+  isAdded,
+  newItem
+)
+
+const store = createStore(reducer);
 
 const comprasApp = () =>{
 
